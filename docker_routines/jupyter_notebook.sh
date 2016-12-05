e0b0b6a4e42e0862618857d6719008e688a421ef
vuchetichbalint <vuchetichbalint@gmail.com>
2016-12-05 21:49:42 +0100

    added root access

--- docker_routines/jupyter_notebook.sh
+++ docker_routines/jupyter_notebook.sh
@@ -9,8 +9,7 @@ passwd()
 
 docker rm -f jupyter_nb
 
-docker run --name jupyter_nb -p 8888:8888 -dit -e USE_HTTPS=true --volume=/home/balint/workspace/kaggle/bosh/input:/data jupyter/all-spark-notebook start-notebook.sh --NotebookApp.password='sha1:80cfc8c0ea27:86c97243229abca32a038e19bcd0e0310d487226'
-docker run --name jupyter_nb -p 8888:8888 -dit -e USE_HTTPS=true jupyter/all-spark-notebook start-notebook.sh --NotebookApp.password='sha1:80cfc8c0ea27:86c97243229abca32a038e19bcd0e0310d487226'
+docker run --name jupyter_nb -p 8888:8888 -dit -e USE_HTTPS=true -e GRANT_SUDO=yes --user root --volume=/home/balint/workspace/kaggle/bosh/input:/data jupyter/all-spark-notebook start-notebook.sh --NotebookApp.password='sha1:6d5aa18a7421:a13e12ad295753e507fed8f7ff7ccc6dd5c710c1'
 
 # now it should work via browser at https://localhost:8888
 
